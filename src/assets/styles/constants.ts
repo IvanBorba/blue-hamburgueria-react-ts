edc952f2f3eede6157135b3ea848b9136bb366e8
Ivan Borba <mrborbaivan@gmail.com>
2022-08-01 20:14:18 -0300

    feature: created the colors and the constants

---
+++ src/assets/styles/constants.ts
@@ -0,0 +1,10 @@
+export const constants = {
+  logoFontFamily: "Big Shoulders Inline Display",
+  bodyFontFamily: "Barlow",
+
+  logoFontSize: "28px",
+  headingFontSize: "28px",
+  bodyFontSize: "14px",
+
+  lineHeight: "140%",
+};
---
+++ src/assets/styles/theme.ts
@@ -0,0 +1,9 @@
+import { colors } from "./colors";
+import { constants } from "./constants";
+
+const theme = {
+  colors,
+  constants,
+} as const;
+
+export default theme;
